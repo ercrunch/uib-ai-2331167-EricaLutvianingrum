16d61d113fc761255d526fddb64319ef51f228b5
Erica <ericalutfia223@gmail.com>
2025-10-07 02:37:12 +0700

    Tugas Minggu 2

---
+++ Tugas_Minggu_1/verify_installation.py
@@ -0,0 +1,15 @@
+import numpy, pandas, matplotlib, seaborn, sklearn, cv2, nltk
+import torch, tensorflow, flask, fastapi
+
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("OpenCV:", cv2.__version__)
+print("NLTK:", nltk.__version__)
+print("PyTorch:", torch.__version__)
+print("TensorFlow:", tensorflow.__version__)
+print("Flask:", flask.__version__)
+print("FastAPI:", fastapi.__version__)
+
+print("\nAll core packages OK!")
